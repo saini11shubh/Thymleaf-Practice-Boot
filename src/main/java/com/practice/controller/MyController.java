5ab3f2f8fdc689c8633df5e32152907b27e25550
Shubham Saini <shubhamsaini86963@gmail.com>
2022-07-01 13:27:36 +0530

    Update project

--- src/main/java/com/practice/controller/MyController.java
+++ src/main/java/com/practice/controller/MyController.java
@@ -12,38 +12,50 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
-	
-	@RequestMapping(value="/about",method=RequestMethod.GET)
-	public String about( Model model) {
+
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public String about(Model model) {
 		System.out.println("Inside about handler...");
-		model.addAttribute("name","Shubham");
+		model.addAttribute("name", "Shubham");
 		model.addAttribute("currentDate", new Date().toLocaleString());
 		return "about";
-		//about.html
+		// about.html
 	}
-	
+
 	@GetMapping("/example-loop")
 	public String iterateHandler(Model model) {
-		List<String> names = List.of("Ankit","laxmi","karan","Johan");
-		model.addAttribute("names",names);
+		List<String> names = List.of("Ankit", "laxmi", "karan", "Johan");
+		model.addAttribute("names", names);
 		return "iterator";
 	}
-	
-	//handler for  conditional statements
+
+	// handler for conditional statements
 	@GetMapping("/condition")
 	public String conditionHandler(Model m) {
 		System.out.print("conditional handler executed..");
-		m.addAttribute("isActive",true);
+		m.addAttribute("isActive", true);
 		m.addAttribute("Gender", "f");
-		List<Integer> mylist = List.of(14,45,485,58,78,55);
-		m.addAttribute("mylist",mylist);
+		List<Integer> mylist = List.of(14, 45, 485, 58, 78, 55);
+		m.addAttribute("mylist", mylist);
 		return "condition";
 	}
-	
+
 	@GetMapping("/fragments")
 	public String fragmentsHandler(Model m) {
-		m.addAttribute("title","I like a samosa");
-		m.addAttribute("subtitle",LocalDateTime.now().toString());
+		m.addAttribute("title", "I like a samosa");
+		m.addAttribute("subtitle", LocalDateTime.now().toString());
 		return "fragment";
+	}
+
+	// for Base and aboutBase
+	@GetMapping("/newabout")
+	public String newAbout() {
+		return "aboutBase";
+	}
+	
+	//for contact 
+	@GetMapping("/contact")
+	public String contact() {
+		return "contact";
 	}
 }
